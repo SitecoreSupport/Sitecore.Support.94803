5a7366c8e44cc489c30da50a3243e93d27bf9bc1
Pavel <piv@sitecore.net>
2017-11-14 10:56:05 +0200

    #94803: Initial commit

---
+++ src/Sitecore.Support.94803/Pipelines/Search/IDResolver.cs
@@ -0,0 +1,28 @@
+﻿namespace Sitecore.Support.Pipelines.Search
+{
+  using System;
+  using Data;
+  using Data.Items;
+  using Diagnostics;
+  using Globalization;
+  using Sitecore.Pipelines.Search;
+  using Sitecore.Search;
+
+  public class IDResolver
+  {
+    public void Process(SearchArgs args)
+    {
+      Guid guid;
+      Assert.ArgumentNotNull(args, "args");
+      if (Guid.TryParse(args.TextQuery, out guid))
+      {
+        Item item = args.Database.GetItem(new ID(guid), args.ContentLanguage);
+        if (item != null)
+        {
+          SearchResult result = SearchResult.FromItem(item);
+          args.Result.AddResultToCategory(result, Translate.Text("Direct Hit"));
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.94803/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94803")]
+[assembly: AssemblyProduct("Sitecore.Support.94803")]
+[assembly: ComVisible(false)]
